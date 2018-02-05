794db818daaaf9e706ce867a2e3bfbe65d3ea7fa
sc0Vu <alk03073135@gmail.com>
2018-02-05 09:11:12 +0800

    Add test

--- test/gametoken.js
+++ test/gametoken.js
@@ -60,4 +60,20 @@ contract('GameToken', function(accounts) {
       assert.equal(balance.valueOf(), 100, "100 Game Token wasn't in the second account");
     });
   });
+
+  it("should transfer 100 GameToken to the second account using transferFrom", function() {
+    var token;
+
+    return GameToken.new(10000, 'Game Token', 1, 'GT', {from: firstAccount}).then(function(instance) {
+      token = instance;
+
+      return token.approve(secondAccount, 100, {from: firstAccount});
+    }).then(function () {
+      return token.transferFrom(firstAccount, secondAccount, 100, {from: secondAccount});
+    }).then(function() {
+      return token.balanceOf.call(secondAccount);
+    }).then(function (balance) {
+      assert.equal(balance.valueOf(), 100, "100 Game Token wasn't in the second account");
+    });
+  });
 });
